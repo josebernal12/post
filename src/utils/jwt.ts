639f21bfce0fd70fc2cdd873d3f3f9c08b2b85fb
jose <jose12_bernal@hotmail.com>
2023-09-27 09:51:14 -0700

    refactor jsonwebtoken

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken'
 import env from '../utils/envalid'
-export const CreateJsonWebToken = (id: string) => {
+export const CreateJsonWebToken = (id: string | undefined) => {
   const token = jwt.sign({ id }, env.PRIVATEKEY)
   return token
 }
