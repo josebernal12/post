2b980653c1c71aea2e0fc1fcf3fdef0ea8b9bbbf
jose <jose12_bernal@hotmail.com>
2023-09-29 10:40:56 -0700

    test user.register added

---
+++ test/user.register.test.ts
@@ -0,0 +1,49 @@
+import request from 'supertest'
+import { faker } from '@faker-js/faker'
+import app from '../src/server'
+describe('registro de un usuario', () => {
+  it('un usuario se registrara satisfactoriamente', (done) => {
+
+    const userData = {
+      name: faker.person.firstName(),
+      email: faker.internet.email(),
+      password: faker.internet.password()
+    }
+    request(app)
+      .post('/api/user/register')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .send(userData)
+      .expect(200, done)
+
+  })
+  it('un usuario no envio todos los campos necesario', (done) => {
+    const userData = {
+      email: "antonio212@hotmail.com",
+      password: "1234"
+    }
+    request(app)
+      .post('/api/user/register')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .send(userData)
+      .expect(404, done)
+  })
+
+  it('el email ya esta en la base de datos', (done) => {
+    const userData = {
+      name: "antonio",
+      email: "antonio@hotmail.com",
+      password: "1234"
+    }
+
+    request(app)
+      .post('/api/user/register')
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .send(userData)
+      .expect(404, done)
+  })
+
+
+})
