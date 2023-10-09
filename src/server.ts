28527ad1d5e8580f66b3f04474ca8301e1815263
jose <jose12_bernal@hotmail.com>
2023-10-09 16:15:14 -0700

    router added

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,13 @@ import 'dotenv/config'
 import sequelize from './database/database'
 import env from './utils/envalid'
 import userRouter from './router/user.router'
+import customerRouter from './router/customer.router'
+
 const app = express()
 const port = env.PORT
 app.use(express.json())
 app.use('/api/user', userRouter)
-
+app.use('/api/customer', customerRouter)
 
 app.listen(port)
 console.log(`server listening to port ${port}`)
